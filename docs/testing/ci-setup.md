7f4fe0582796ce35b5dc3a0998c81e81ff5cb1f4
Alon Negrin <anegrin@nvidia.com>
2024-07-18 08:21:57 -0700

    Added gitlab_runner.sh script to register CI runners

--- docs/testing/ci-setup.md
+++ docs/testing/ci-setup.md
@@ -16,7 +16,6 @@ This project uses two types of gitlab runners:
   - user gitlab-runner with docker groups
   - cache directory at `/mnt/gitlab-runner/cache` which is mounted into the container through the config.toml.
 
-
 The config.toml for the docker runner is located at /srv/gitlab-runner/config/config.toml on the host by default. It is shared between all docker containers which run jobs. It looks like:
 ```toml
 concurrent = 10
@@ -61,7 +60,7 @@ shutdown_timeout = 0
     - A go version matching the version in the go.mod
     - Run the docker container from https://github.com/tonistiigi/binfmt to install necessary dependencies for building arm images.
     - Secrets for any registry the release runner is required to push artifacts to e.g. harbour, nvstaging
-    - 
+    -
 The config.toml for the e2e runner is located at /etc/gitlab-runner/config.toml by default. It looks like:
 ```toml
 concurrent = 1
@@ -87,70 +86,27 @@ shutdown_timeout = 0
 The below set of bash commands can be used to setup a new gitlab runner on ubuntu.
 Note: The below is for illustration only and is not intended as a runnable script.
 All commands are run as root from /root unless otherwise specified.
+to run the script you will need to provide the environment with the tokens listed below.
+Note: in case of using a leased setup from Colossus you can use root user by using `sudo su`
+after you logged in with the user who made the lease.
+* use OS: ubuntu-22.04-x86_64-standard
+Link to colossus lease:
+- https://colossus.nvidia.com/web/index.html#/schedule
 
 ```shell
-## Suspend the ubuntu needrestart from interrupting installs
-export NEEDRESTART_SUSPEND=true
+# Make sure you have these valid tokens variables set on the host
+#
 
-## Add the gitlab-runner user
-sudo useradd -m -d /gitlab-runner gitlab-runner
+# Should have gitlab group owner permissions for creating a group runner.
+export GROUP_ACCESS_TOKEN="<group_access_token>"
 
-## install kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+# Should have gitlab maintainer permissions for dpf project to login to the repo.
+export PERSONAL_ACCESS_TOKEN="<personal_access_token>"
 
-## Install docker
-sudo apt-get update
-sudo apt-get install ca-certificates curl lftp
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+# need to login to nvcr.io to pull/push images.
+export NGC_API_KEY="<ngc_api_key>"
 
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-## Add the gitlab runner to the docker group
-sudo usermod  -aG docker gitlab-runner
-
-## Install go
-wget https://go.dev/dl/go1.22.3.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
-## Set the go path for the gitlab-runner
-echo PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/bin:/usr/local/go/bin" > /gitlab-runner/.bashrc
-
-## Enable docker building with qemu for multi-arch
-docker run --privileged --rm tonistiigi/binfmt --install all
-
-
-## Set the inotify limits for the host.
-## This fixes a common "too many open files" error on some systems.
-sudo sysctl fs.inotify.max_user_watches=1048576
-sudo sysctl fs.inotify.max_user_instances=8192
-
-## Delete .bash_logout
-sudo rm -rf /gitlab-runner/.bash_logout
-
-## Download the gitlab runner binary
-sudo curl -L --output /usr/local/bin/gitlab-runner "https://s3.dualstack.us-east-1.amazonaws.com/gitlab-runner-downloads/v17.1.0/binaries/gitlab-runner-linux-amd64"
-sudo chmod +x /usr/local/bin/gitlab-runner
-
-## install gitlab runner and register 
-sudo gitlab-runner install --user=gitlab-runner --working-directory=/gitlab-runner
-
-sudo gitlab-runner start
-
-## Runner must be registered in the CI as a shell runner with command from gitlab UI.
-gitlab-runner register XXXX 
-
-## Must be done with credentials that allow pushing to the given registry.
-## Must be done as the gitlab-runner user.
-su gitlab-runner
-docker login nvcr.io #Note. This is an interactive command and needs to be done manually.
-docker login gitlab-master.nvidia.com:5005 #Note. This is an interactive command and needs to be done manually.
+# Copy the gitlab_runner.sh script to your machine inside /root directory.
+https://gitlab-master.nvidia.com/doca-platform-foundation/dpf-operator/-/blob/main/hack/scripts/gitlab_runner.sh?ref_type=heads
 
 ```
---
+++ hack/scripts/gitlab_runner.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+#  2024 NVIDIA CORPORATION & AFFILIATES
+#
+#  Licensed under the Apache License, Version 2.0 (the License);
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an AS IS BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+LOGFILE="setup_log.txt"
+exec > >(tee -i $LOGFILE)
+exec 2>&1
+
+log_and_exit() {
+    echo "$1" >&2
+    exit 1
+}
+
+## Check for required variables
+if [ -z "$GROUP_ACCESS_TOKEN" ] || [ -z "$PERSONAL_ACCESS_TOKEN" ] || [ -z "$NGC_API_KEY" ]; then
+    log_and_exit "All GROUPL_ACCESS_TOKEN, PERSONAL_ACCESS_TOKEN and NGC_API_KEY variables are required"
+fi
+
+## GLOBAL VARS
+GROUP_ID="151554"
+SHELL_TAG_LIST="type/shell,e2e,release"
+DOCKER_TAG_LIST="type/docker"
+
+## Install JQ for json parsing
+sudo apt-get install -y -qq jq > /dev/null || log_and_exit "Failed to download jq"
+
+## Suspend the ubuntu needrestart from interrupting installs
+export NEEDRESTART_SUSPEND=true
+
+## Add the gitlab-runner user
+sudo useradd -m -d /gitlab-runner gitlab-runner || log_and_exit "Failed to add gitlab-runner user"
+
+## Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" > /dev/null || log_and_exit "Failed to download kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl || log_and_exit "Failed to install kubectl"
+
+## Install docker
+sudo apt-get update > /dev/null || log_and_exit "Failed to update package list"
+sudo apt-get install -y ca-certificates curl lftp > /dev/null || log_and_exit "Failed to install required packages"
+sudo install -m 0755 -d /etc/apt/keyrings || log_and_exit "Failed to create /etc/apt/keyrings directory"
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc || log_and_exit "Failed to download Docker GPG key"
+sudo chmod a+r /etc/apt/keyrings/docker.asc || log_and_exit "Failed to set permissions on Docker GPG key"
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null || log_and_exit "Failed to add Docker repository"
+sudo apt-get update > /dev/null || log_and_exit "Failed to update package list after adding Docker repository"
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null || log_and_exit "Failed to install Docker packages"
+
+## Add the gitlab runner to the docker group
+sudo usermod -aG docker gitlab-runner || log_and_exit "Failed to add gitlab-runner to docker group"
+
+## Install Go
+sudo wget https://go.dev/dl/go1.22.3.linux-amd64.tar.gz > /dev/null || log_and_exit "Failed to download Go"
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz || log_and_exit "Failed to extract Go archive"
+echo 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/bin:/usr/local/go/bin"' | sudo tee /gitlab-runner/.bashrc > /dev/null || log_and_exit "Failed to set Go path for gitlab-runner"
+
+## Enable Docker building with qemu for multi-arch
+sudo docker run --privileged --rm tonistiigi/binfmt --install all > /dev/null || log_and_exit "Failed to enable Docker building with qemu"
+
+## Set the inotify limits for the host
+sudo sysctl fs.inotify.max_user_watches=1048576 > /dev/null || log_and_exit "Failed to set fs.inotify.max_user_watches"
+sudo sysctl fs.inotify.max_user_instances=8192 > /dev/null || log_and_exit "Failed to set fs.inotify.max_user_instances"
+
+## Delete .bash_logout
+sudo rm -rf /gitlab-runner/.bash_logout > /dev/null || log_and_exit "Failed to delete /gitlab-runner/.bash_logout"
+
+## Download the gitlab runner binary
+sudo curl -L --output /usr/local/bin/gitlab-runner "https://s3.dualstack.us-east-1.amazonaws.com/gitlab-runner-downloads/v17.1.0/binaries/gitlab-runner-linux-amd64" > /dev/null || log_and_exit "Failed to download gitlab-runner binary"
+sudo chmod +x /usr/local/bin/gitlab-runner || log_and_exit "Failed to make gitlab-runner binary executable"
+
+## Install gitlab runner and register
+sudo gitlab-runner install --user=gitlab-runner --working-directory=/gitlab-runner || log_and_exit "Failed to install gitlab-runner"
+sudo gitlab-runner start || log_and_exit "Failed to start gitlab-runner"
+
+## Runner must be registered in the CI as a shell runner with command from gitlab UI.
+## Please run: gitlab-runner register
+
+## Register shell runner
+register_token=$(curl --silent --request POST --url "https://gitlab-master.nvidia.com/api/v4/user/runners" \
+  --data "runner_type=group_type" \
+  --data "group_id=$GROUP_ID" \
+  --data "description=$(hostname) - $(hostname -I | awk '{print $1}')" \
+  --data "tag_list=$SHELL_TAG_LIST" \
+  --data "run_untagged=false" \
+  --header "PRIVATE-TOKEN: $GROUP_ACCESS_TOKEN" | jq -r '.token')
+
+gitlab-runner register  --non-interactive \
+  --url https://gitlab-master.nvidia.com \
+  --token "$register_token" \
+  --executor="shell" || log_and_exit "Failed to register shell gitlab-runner"
+
+
+## Register docker runner
+register_token=$(curl --silent --request POST --url "https://gitlab-master.nvidia.com/api/v4/user/runners" \
+  --data "runner_type=group_type" \
+  --data "group_id=$GROUP_ID" \
+  --data "description=$(hostname) - $(hostname -I | awk '{print $1}')" \
+  --data "tag_list=$DOCKER_TAG_LIST" \
+  --data "run_untagged=true" \
+  --header "PRIVATE-TOKEN: $GROUP_ACCESS_TOKEN" | jq -r '.token')
+
+gitlab-runner register  --non-interactive \
+  --url https://gitlab-master.nvidia.com \
+  --token "$register_token" \
+  --executor="docker" \
+  --docker-tlsverify=false \
+  --docker-image golang:1.22.0 \
+  --docker-privileged=false \
+  --docker-disable-entrypoint-overwrite=false \
+  --docker-oom-kill-disable=false \
+  --docker-disable-cache=false \
+  --docker-volumes="/mnt/gitlab-runner/cache:/cache" \
+  --docker-shm-size=0 \
+  --docker-network-mtu=0 \
+  --docker-cpus="4" \
+  --docker-memory="4000000000" || log_and_exit "Failed to register docker gitlab-runner"
+
+## Must be done with credentials that allow pushing to the given registry.
+## Must be done as the gitlab-runner user.
+echo "Switching to gitlab-runner user for Docker login"
+echo "$PERSONAL_ACCESS_TOKEN" | sudo -u gitlab-runner docker login --username \$oauthtoken --password-stdin gitlab-master.nvidia.com:5005 || log_and_exit "Failed to login to gitlab repo"
+echo "$NGC_API_KEY" | sudo -u gitlab-runner docker login --username \$oauthtoken --password-stdin nvcr.io || log_and_exit "Failed to login to nvcr.io"
+
+# Done
+echo "GitLab runner setup completed successfully"
