9283165e8cc6493e9e4d5c4abaf3b06b4663b62b
Igor Ivanov <igori@nvidia.com>
2024-09-17 16:12:57 +0300

    feat: add dpuset controller test
    Signed-off-by: Igor Ivanov <igori@nvidia.com>

---
+++ internal/provisioning/controllers/dpuset_controller_test.go
@@ -0,0 +1,159 @@
+/*
+Copyright 2024 NVIDIA
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package provisioning_controller
+
+import (
+	"context"
+	"time"
+
+	provisioningv1 "gitlab-master.nvidia.com/doca-platform-foundation/doca-platform-foundation/api/provisioning/v1alpha1"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/yaml"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// These tests are written in BDD-style using Ginkgo framework. Refer to
+// http://onsi.github.io/ginkgo to learn more.
+var _ = Describe("Dpu", func() {
+
+	const (
+		DefaultNS   = "dpf-provisioning-test"
+		DefaultNode = "dpf-provisioning-node-test"
+		DefaultBfb  = "dpf-provisioning-bfb-test"
+	)
+
+	var (
+		testNS   *corev1.Namespace
+		testNode *corev1.Node
+	)
+
+	var getObjKey = func(obj *provisioningv1.DpuSet) types.NamespacedName {
+		return types.NamespacedName{
+			Name:      obj.Name,
+			Namespace: obj.Namespace,
+		}
+	}
+
+	var createObj = func(name string) *provisioningv1.DpuSet {
+		return &provisioningv1.DpuSet{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: testNS.Name,
+			},
+			Spec:   provisioningv1.DpuSetSpec{},
+			Status: provisioningv1.DpuSetStatus{},
+		}
+	}
+
+	var createNode = func(ctx context.Context, name string, labels map[string]string) *corev1.Node {
+		node := &corev1.Node{ObjectMeta: metav1.ObjectMeta{Name: name, Labels: labels}}
+		Expect(k8sClient.Create(ctx, node)).NotTo(HaveOccurred())
+		return node
+	}
+
+	BeforeEach(func() {
+		By("creating the namespace")
+		testNS = &corev1.Namespace{ObjectMeta: metav1.ObjectMeta{GenerateName: DefaultNS}}
+		Expect(client.IgnoreAlreadyExists(k8sClient.Create(ctx, testNS))).To(Succeed())
+
+		By("creating the node")
+		testNode = createNode(ctx, DefaultNode, make(map[string]string))
+	})
+
+	AfterEach(func() {
+		By("deleting the namespace")
+		Expect(k8sClient.Delete(ctx, testNS)).To(Succeed())
+
+		By("Cleaning the node")
+		Expect(k8sClient.Delete(ctx, testNode)).To(Succeed())
+	})
+
+	Context("obj test context", func() {
+		ctx := context.Background()
+
+		It("create and destroy", func() {
+			By("creating the obj")
+			obj := createObj("obj-dpuset")
+			Expect(k8sClient.Create(ctx, obj)).To(Succeed())
+			DeferCleanup(k8sClient.Delete, ctx, obj)
+
+			obj_fetched := &provisioningv1.DpuSet{}
+
+			By("checking the finalizer")
+			Eventually(func(g Gomega) []string {
+				g.Expect(k8sClient.Get(ctx, getObjKey(obj), obj_fetched)).To(Succeed())
+				return obj_fetched.Finalizers
+			}).WithTimeout(10 * time.Second).Should(ConsistOf([]string{provisioningv1.DpuSetFinalizer}))
+		})
+
+		It("create from yaml", func() {
+			yml := []byte(`
+apiVersion: provisioning.dpf.nvidia.com/v1alpha1
+kind: DpuSet
+metadata:
+  name: dpuset-1
+  namespace: default
+spec:
+  nodeSelector:
+    matchLabels:
+      feature.node.kubernetes.io/dpu-enabled: "true"
+  dpuSelector:
+      feature.node.kubernetes.io/dpu-0-psid: "MT_0000000375"
+      feature.node.kubernetes.io/dpu-0-pci-address: "0000-04-00"
+  strategy:
+    rollingUpdate:
+      maxUnavailable: 10%
+    type: RollingUpdate
+  dpuTemplate:
+    annotations:
+      nvidia.com/dpuOperator-override-powercycle-command: "cycle"
+    spec:
+      dpuFlavor: "hbn"
+      BFB:
+        bfb: "doca-24.04"
+      nodeEffect:
+        taint:
+          key: "dpu"
+          value: "provisioning"
+          effect: NoSchedule
+      k8s_cluster:
+        name: "tenant-00"
+        namespace: "tenant-00-ns"
+        node_labels:
+          "dpf.node.dpu/role": "worker"
+`)
+			obj := &provisioningv1.DpuSet{}
+			err := yaml.UnmarshalStrict(yml, obj)
+			Expect(err).To(Succeed())
+			Expect(k8sClient.Create(ctx, obj)).To(Succeed())
+			DeferCleanup(k8sClient.Delete, ctx, obj)
+
+			obj_fetched := &provisioningv1.DpuSet{}
+
+			By("checking the finalizer")
+			Eventually(func(g Gomega) []string {
+				g.Expect(k8sClient.Get(ctx, getObjKey(obj), obj_fetched)).To(Succeed())
+				return obj_fetched.Finalizers
+			}).WithTimeout(10 * time.Second).Should(ConsistOf([]string{provisioningv1.DpuSetFinalizer}))
+		})
+	})
+})
