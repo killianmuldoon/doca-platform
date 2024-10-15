83592766f6056838eb606c9f9858e6eadd385ed1
Soule Ba <souleb@nvidia.com>
2024-10-15 01:15:39 -0700

    Enable caching instructured objects

--- cmd/dpuservice/main.go
+++ cmd/dpuservice/main.go
@@ -39,6 +39,7 @@ import (
 	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
@@ -128,6 +129,11 @@ func main() {
 		Metrics:                metricsOpts,
 		WebhookServer:          webhookServer,
 		HealthProbeBindAddress: probeAddr,
+		Client: client.Options{
+			Cache: &client.CacheOptions{
+				Unstructured: true,
+			},
+		},
 		Cache: cache.Options{
 			SyncPeriod: &syncPeriod,
 		},
