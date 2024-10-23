08b23dbcdb2e7b1e7943fc18c78d16748289b711
Killian Muldoon <kmuldoon@nvidia.com>
2024-10-23 04:02:29 -0700

    Reduce notifications from release CI

--- hack/scripts/slack-notification.sh
+++ hack/scripts/slack-notification.sh
@@ -23,7 +23,10 @@ set -o errexit
 E2E_TEST="${E2E_TEST:-""}"
 PIPELINE_NAME="${PIPELINE_NAME:-"${CI_PIPELINE_NAME}"}"
 
-## Ignore if the pipeline is not from a schedule or not from a "push" - i.e. when a new commit is added to main.
+# This limit is set with the DPF release job in mind. This job currently runs up to 3 times.
+limit="${RETRY_LIMIT:-3}"
+
+# Ignore if the pipeline is not from a schedule or not from a "push" - i.e. when a new commit is added to main.
 if [[ "$CI_PIPELINE_SOURCE" != "schedule" && "$CI_PIPELINE_SOURCE" != "push" ]]; then
   echo "Skipping slack notification: this is not a scheduled or push pipeline"
   exit 0
@@ -35,10 +38,22 @@ if [[ "$CI_JOB_STATUS" == "success" ]]; then
   exit 0
 fi
 
+
+## Exit if this is a release job and it is still being retried.
+if [[ $CI_JOB_NAME == "release" ]]; then
+  echo "looking at the retries now"
+  pipeline_info=$(curl -sL --header "PRIVATE-TOKEN: $GITLAB_API_TOKEN" "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/pipelines/${CI_PIPELINE_ID}/jobs?include_retried=true")
+  num_retries=$(echo "$pipeline_info" | jq --exit-status --arg name "$CI_JOB_NAME" 'map(.name) | map(select(. == $name)) | length')
+  if [ ${num_retries} -lt ${limit} ]; then
+    echo "Skipping slack notification: retries ${num_retries} do not exceed limit ${limit}."
+    exit 0
+  fi
+fi
+
 notification_message="Pipeline ${PIPELINE_NAME} has failed.\n\nDetails: ${CI_PIPELINE_URL}"
 
 if [ "$E2E_TEST" == "true" ]; then
-  notification_message="${notification_message}\n\nArtifacts download:\ncurl -L -o ${CI_JOB_ID}.zip --header \\\"PRIVATE-TOKEN: \$GITLAB_API_TOKEN\\\" https://gitlab-master.nvidia.com/api/v4/jobs/${CI_JOB_ID}/artifacts && unzip -d ${CI_JOB_ID} ${CI_JOB_ID}.zip && rm ${CI_JOB_ID}.zip"
+  notification_message="${notification_message}\n\nArtifacts download:\ncurl -L -o ${CI_JOB_ID}.zip --header \\\"PRIVATE-TOKEN: \$GITLAB_API_TOKEN\\\" ${CI_API_V4_URL}µ/jobs/${CI_JOB_ID}/artifacts && unzip -d ${CI_JOB_ID} ${CI_JOB_ID}.zip && rm ${CI_JOB_ID}.zip"
 fi
 
 echo "{\"notification_message\":${notification_message}\"}"
