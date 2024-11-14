d55e5a552c8e4e5286f4fadd0854e7e434ad4a81
Killian Muldoon <kmuldoon@nvidia.com>
2024-11-14 03:33:45 -0800

    Default leader election to true for DPUService controller

--- cmd/dpuservice/main.go
+++ cmd/dpuservice/main.go
@@ -76,7 +76,7 @@ func main() {
 	var syncPeriod time.Duration
 	fs.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	fs.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	fs.BoolVar(&enableLeaderElection, "leader-elect", false,
+	fs.BoolVar(&enableLeaderElection, "leader-elect", true,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
 	fs.BoolVar(&insecureMetrics, "insecure-metrics", false,
